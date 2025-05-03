33e9901d6db438b2c3051567e1e994565d5620a2
Sofus Laubel <sofus@laubel.net>
2025-05-03 22:21:40 +0200

    easy command to run all

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ a = np.array([[1, 2], [3, 4]])
 
 a[0:2, 0:2] *= 2
 print(a)
+
+
+
+
+# python src/run_HDM_teeth_CPU.py && python src/run_HDM_teeth_GPU_CuPy.py && python src/run_HDM_teeth_GPU_PyTorch.py && python src/run_HDM_teeth_GPU_PyTorch_CuPy.py && python src/run_HDM_wings_CPU.py && python src/run_HDM_wings_GPU_CuPy.py 
