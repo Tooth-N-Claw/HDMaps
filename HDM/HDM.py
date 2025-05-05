5404d80374da585d7550b50b46800b8758407ff4
Felix Risbro Hjerrild <73933966+frisbro303@users.noreply.github.com>
2025-05-05 10:31:11 +0200

    Update HDM.py

--- HDM/HDM.py
+++ HDM/HDM.py
@@ -17,18 +17,18 @@ def run_HDM(backend, hdm_config, hdm_data):
     match backend:
         case "cpu":
             print("Running HDM on CPU")
-            from backends.HDM_CPU import run_hdm_cpu 
+            from HDM.backends.HDM_CPU import run_hdm_cpu 
             return run_hdm_cpu(hdm_config, hdm_data)
         case "gpu_pytorch":
-            from backends.HDM_GPU_PyTorch import run_hdm_gpu # import here to avoid torch being loaded unnecessarily, meaning you can run the code without having torch installed
+            from HDM.backends.HDM_GPU_PyTorch import run_hdm_gpu # import here to avoid torch being loaded unnecessarily, meaning you can run the code without having torch installed
             return run_hdm_gpu(hdm_config, hdm_data)
         case "gpu_pytorch_cupy":
             # This is a speed up for pytorch as it does not have fast eigendecomposition yet for sparse matrix, so we use cupy instead. Though it has a overhead of converting the sparse matrix to cupy and back again
             hdm_config.use_cupy = True
-            from backends.HDM_GPU_PyTorch import run_hdm_gpu
+            from HDM.backends.HDM_GPU_PyTorch import run_hdm_gpu
             return run_hdm_gpu(hdm_config, hdm_data)
         case "gpu_cupy":
-            from backends.HDM_GPU_CuPy import run_hdm_cupy
+            from HDM.backends.HDM_GPU_CuPy import run_hdm_cupy
             return run_hdm_cupy(hdm_config, hdm_data)
         
           
