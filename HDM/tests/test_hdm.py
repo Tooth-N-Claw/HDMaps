381457515238c02980412bf76ddc52783e35ea42
wush303 <felix@risbro.dk>
2025-07-12 02:16:41 +0200

    Added joint_kernel test

--- HDM/tests/test_hdm.py
+++ HDM/tests/test_hdm.py
@@ -1,35 +1,40 @@
-
-
-
+import pytest
 import jax.numpy as jnp
 from jax.experimental.sparse import BCOO
-
 from jax.experimental import sparse
-import numpy as np
 
-
-base_kernel = jnp.array(
-    [[1, 0],
-     [1, 1]]
-)
-base_kernel = sparse.BCOO.fromdense(base_kernel)
-
-fiber_kernel = jnp.array(
-    [[1, 0, 0, 0],
-     [0, 1, 1, 0],
-     [0, 0, 0, 0],
-     [0, 1, 1, 1]]
-)
-fiber_kernel = sparse.BCOO.fromdense(fiber_kernel)
-
-block_indices = jnp.array([0, 2])
 
 from HDM.kernels import compute_joint_kernel
 
-joint_kernel = compute_joint_kernel(base_kernel, fiber_kernel, block_indices)
 
-print("what")
-print(block_indices)
-print(base_kernel.todense())
-print(fiber_kernel.todense())
-print(joint_kernel.todense())
+def test_compute_joint_kernel():
+
+    base_kernel = jnp.array(
+        [[1, 0],
+         [1, 1]]
+    )
+    base_kernel = sparse.BCOO.fromdense(base_kernel)
+
+    fiber_kernel = jnp.array(
+        [[1, 0, 0, 0],
+         [0, 1, 1, 0],
+         [0, 0, 0, 0],
+         [0, 1, 1, 1]]
+    )
+    fiber_kernel = sparse.BCOO.fromdense(fiber_kernel)
+
+    block_indices = jnp.array([0, 2])
+
+    joint_kernel = compute_joint_kernel(base_kernel, fiber_kernel, block_indices)
+    joint_kernel = joint_kernel.todense()
+
+    expected_joint_kernel = jnp.array(
+        [[1, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 0, 0, 0],
+         [0, 1, 1, 1]]
+    )
+
+    assert jnp.array_equal(joint_kernel, expected_joint_kernel), (  
+        "Joint kernel output doesn't match expected output"
+    )
