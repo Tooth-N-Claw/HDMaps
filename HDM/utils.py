fd433f21408c0c6c5e4b2e7e956fae6284de71b5
wush303 <felix@risbro.dk>
2025-07-17 16:25:39 +0200

    Fixed import

--- HDM/utils.py
+++ HDM/utils.py
@@ -1,7 +1,5 @@
 import numpy as np
 from typing import NamedTuple
-
-from jax.experimental.sparse import BCOO
 from scipy.sparse import csr_matrix
 
 
@@ -18,5 +16,5 @@ class HDMConfig(NamedTuple):
     
 def compute_block_indices(data_samples: list[np.ndarray]) -> np.ndarray:
     """Compute cumulative start indices for a list of data samples."""
-    lengths = np.array([len(s) for s in data_samples], dtype=jnp.int32)
+    lengths = np.array([len(s) for s in data_samples], dtype=np.int32)
     return np.concatenate([np.array([0], dtype=np.int32), np.cumsum(lengths)])
