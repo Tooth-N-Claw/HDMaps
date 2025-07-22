d6af448069a9c1dd11ffe6c6d33ff80fbd8f21fe
wush303 <felix@risbro.dk>
2025-07-22 13:00:17 -0400

    Added cluster function

--- HDM/__init__.py
+++ HDM/__init__.py
@@ -1,5 +1,11 @@
 from .HDM import hdm_embed
-from .utils import HDMConfig
+from .utils import HDMConfig, compute_clusters
 from .spatial import compute_fiber_kernel_from_maps
 
-__all__ = ['hdm_embed', 'HDMConfig', 'compute_fiber_kernel_from_maps']
+
+__all__ = [
+    'hdm_embed',
+    'HDMConfig',
+    'compute_fiber_kernel_from_maps',
+    'compute_clusters'
+]
