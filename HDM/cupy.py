b1903b9aecfdd01e958b149d98c8b58cf74268e5
Sofus Laubel <sofus@laubel.net>
2025-07-25 11:38:54 -0400

    fixed merge problem

--- HDM/cupy.py
+++ HDM/cupy.py
@@ -1,17 +1,16 @@
 import numpy as np
-
-
 import cupy as cp
-from cupyx.scipy.sparse import coo_matrix, csr_matrix
-from cupyx.scipy import sparse
-
+import cupyx.scipy.sparse as cp_sparse
+import cupyx.scipy.sparse.linalg as cp_linalg
+import scipy.sparse as scipy_sparse
 
 
 def compute_joint_kernel(
-    base_kernel: scipy.sparse.csr_matrix,
-    fiber_kernel: scipy.sparse.coo_matrix,
+    base_kernel: scipy_sparse.csr_matrix,
+    fiber_kernel: scipy_sparse.coo_matrix,
     block_indices: np.ndarray
-) -> coo_matrix:
+) -> cp_sparse.coo_matrix:
+    # Keep CPU operations until COO matrix construction
     fiber_base_row = np.searchsorted(block_indices, fiber_kernel.row, side='right') - 1
     fiber_base_col = np.searchsorted(block_indices, fiber_kernel.col, side='right') - 1
 
@@ -19,11 +18,15 @@ def compute_joint_kernel(
     
     joint_data = fiber_kernel.data * block_vals
     
-    # joint_kernel = coo_matrix((joint_data, (fiber_kernel.row, fiber_kernel.col)), shape=fiber_kernel.shape)
-    joint_kernel = coo_matrix(
-    (joint_data, (fiber_kernel.row, fiber_kernel.col)), 
-    shape=fiber_kernel.shape
-)
+    # Convert to CuPy from here
+    joint_data_gpu = cp.asarray(joint_data)
+    fiber_row_gpu = cp.asarray(fiber_kernel.row)
+    fiber_col_gpu = cp.asarray(fiber_kernel.col)
+    
+    joint_kernel = cp_sparse.coo_matrix(
+        (joint_data_gpu, (fiber_row_gpu, fiber_col_gpu)), 
+        shape=fiber_kernel.shape
+    )
     joint_kernel.eliminate_zeros()
     
     return joint_kernel
@@ -33,81 +36,18 @@ def symmetrize(mat):
     return (mat + mat.T) / 2
 
 
-def normalize_kernel(diffusion_matrix: coo_matrix) -> csr_matrix:
-    row_sums = np.array(diffusion_matrix.sum(axis = 1)).flatten()
-    inv_sqrt_diag = 1 / np.sqrt(row_sums)
-
-    new_data = diffusion_matrix.data * inv_sqrt_diag[diffusion_matrix.row] * inv_sqrt_diag[diffusion_matrix.col]
-    
-    normalized_kernel = csr_matrix((new_data, (diffusion_matrix.row, diffusion_matrix.col)), shape=diffusion_matrix.shape)    
-
-def symmetrize(mat):
-    return (mat + mat.T) / 2
-
-
-def normalize_kernel(diffusion_matrix: coo_matrix) -> csr_matrix:
-    row_sums = cp.array(diffusion_matrix.sum(axis = 1)).flatten()
+def normalize_kernel(diffusion_matrix: cp_sparse.coo_matrix) -> tuple[cp_sparse.csr_matrix, cp.ndarray]:
+    row_sums = cp.asarray(diffusion_matrix.sum(axis=1)).flatten()
     inv_sqrt_diag = 1 / cp.sqrt(row_sums)
 
     new_data = diffusion_matrix.data * inv_sqrt_diag[diffusion_matrix.row] * inv_sqrt_diag[diffusion_matrix.col]
     
-    normalized_kernel = csr_matrix((new_data, (diffusion_matrix.row, diffusion_matrix.col)), shape=diffusion_matrix.shape)    
-
+    normalized_kernel = cp_sparse.csr_matrix(
+        (new_data, (diffusion_matrix.row, diffusion_matrix.col)), 
+        shape=diffusion_matrix.shape
+    )    
 
     normalized_kernel = symmetrize(normalized_kernel)
-    
-    return normalized_kernel, inv_sqrt_diag
-
-
-def eigendecomposition(
-    config,
-    matrix,
-) -> tuple[np.ndarray, np.ndarray]:
-    """Perform eigendecomposition on a sparse matrix."""
-    device = config.device
-    tol = 1e-10
-    maxiter = 10000
-    k = config.num_eigenvectors
-    which = "LM"
-
-    # if device == "cpu":
-    eigvals, eigvecs = sparse.linalg.eigsh(
-        matrix, 
-        k=k, 
-        which=which, 
-        maxiter=maxiter, 
-        tol=tol
-    )
-    # elif device == "cuda":
-    #     with cupy.cuda.Device(int(device.split(":")[-1])):
-    #         cupy_matrix = cupyx.scipy.sparse.csr_matrix(matrix)
-    #         eigvals, eigvecs = cupyx.scipy.sparse.linalg.eigsh(
-    #             cupy_matrix,
-    #             k=k,
-    #             which=which,
-    #             maxiter=maxiter,
-    #             tol=tol
-    #         )
-    #         eigvals, eigvecs = cupy.asnumpy(eigvals), cupy.asnumpy(eigvecs)
-    # else:
-    #     raise ValueError(f"Unsupported device: {device}")    
-
-    # Sort in descending order
-    eigvals = eigvals[::-1]
-    eigvecs = eigvecs[:, ::-1]
-    
-    return eigvals, eigvecs
-
-
-def spectral_embedding(
-    config: HDMConfig,
-    kernel: csr_matrix,
-    inv_sqrt_diag: cp.ndarray,
-):
-
-    sqrt_diag = sparse.diags(inv_sqrt_diag, 0)
-
-    eigvals, eigvecs = eigendecomposition(config, kernel)
     
     return normalized_kernel, inv_sqrt_diag
 
@@ -134,14 +74,21 @@ def eigendecomposition(
     eigvals = eigvals[::-1]
     eigvecs = eigvecs[:, ::-1]
     
-    bundle_HDM = sqrt_diag @ eigvecs[:, 1:]
-    sqrt_lambda = sparse.diags(cp.sqrt(eigvals[1:]), 0)
+    return eigvals, eigvecs
 
+
+def spectral_embedding(
+    config,  # Removed HDMConfig type hint to be more general
+    kernel: cp_sparse.csr_matrix,
+    inv_sqrt_diag: cp.ndarray,
+) -> cp.ndarray:
+    sqrt_diag = cp_sparse.diags(inv_sqrt_diag, 0)
+
+    eigvals, eigvecs = eigendecomposition(config, kernel)
+    
+    bundle_HDM = sqrt_diag @ eigvecs[:, 1:]
+    sqrt_lambda = cp_sparse.diags(cp.sqrt(eigvals[1:]), 0)
     bundle_HDM_full = bundle_HDM @ sqrt_lambda
+    bundle_HDM_full = cp.asnumpy(bundle_HDM_full)  # Convert to NumPy array for consistency
 
     return bundle_HDM_full
-
-
-
-
-
