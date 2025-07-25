e248dd5e74bbdb63dcc44bad539e4221e6095317
Sofus Laubel <sofus@laubel.net>
2025-07-25 17:31:31 -0400

    gpu joint kernel

--- HDM/cupy.py
+++ HDM/cupy.py
@@ -5,31 +5,65 @@ import cupyx.scipy.sparse.linalg as cp_linalg
 import scipy.sparse as scipy_sparse
 
 
-def compute_joint_kernel(
-    base_kernel: scipy_sparse.csr_matrix,
-    fiber_kernel: scipy_sparse.coo_matrix,
+# def compute_joint_kernel(
+#     base_kernel: scipy_sparse.csr_matrix,
+#     fiber_kernel: scipy_sparse.coo_matrix,
+#     block_indices: np.ndarray
+# ) -> cp_sparse.coo_matrix:
+#     # Keep CPU operations until COO matrix construction
+#     fiber_base_row = np.searchsorted(block_indices, fiber_kernel.row, side='right') - 1
+#     fiber_base_col = np.searchsorted(block_indices, fiber_kernel.col, side='right') - 1
+
+#     block_vals = np.array(base_kernel[fiber_base_row, fiber_base_col]).reshape(-1)
+    
+#     joint_data = fiber_kernel.data * block_vals
+    
+#     # Convert to CuPy from here
+#     joint_data_gpu = cp.asarray(joint_data)
+#     fiber_row_gpu = cp.asarray(fiber_kernel.row)
+#     fiber_col_gpu = cp.asarray(fiber_kernel.col)
+    
+#     joint_kernel = cp_sparse.coo_matrix(
+#         (joint_data_gpu, (fiber_row_gpu, fiber_col_gpu)), 
+#         shape=fiber_kernel.shape
+#     )
+#     joint_kernel.eliminate_zeros()
+    
+#     return joint_kernel
+
+
+def compute_joint_kernel_cupy(
+    base_kernel,
+    fiber_kernel,
     block_indices: np.ndarray
 ) -> cp_sparse.coo_matrix:
-    # Keep CPU operations until COO matrix construction
-    fiber_base_row = np.searchsorted(block_indices, fiber_kernel.row, side='right') - 1
-    fiber_base_col = np.searchsorted(block_indices, fiber_kernel.col, side='right') - 1
-
-    block_vals = np.array(base_kernel[fiber_base_row, fiber_base_col]).reshape(-1)
+    """GPU-accelerated version using CuPy"""
     
-    joint_data = fiber_kernel.data * block_vals
-    
-    # Convert to CuPy from here
-    joint_data_gpu = cp.asarray(joint_data)
+    # Transfer data to GPU
     fiber_row_gpu = cp.asarray(fiber_kernel.row)
     fiber_col_gpu = cp.asarray(fiber_kernel.col)
+    fiber_data_gpu = cp.asarray(fiber_kernel.data)
+    block_indices_gpu = cp.asarray(block_indices)
     
-    joint_kernel = cp_sparse.coo_matrix(
-        (joint_data_gpu, (fiber_row_gpu, fiber_col_gpu)), 
+    # Convert base kernel to GPU
+    base_kernel_gpu = cp_sparse.csr_matrix(base_kernel)
+    
+    fiber_base_row = cp.searchsorted(block_indices_gpu, fiber_row_gpu, side='right') - 1
+    fiber_base_col = cp.searchsorted(block_indices_gpu, fiber_col_gpu, side='right') - 1
+    
+    block_vals = base_kernel_gpu[fiber_base_row, fiber_base_col].A1
+    
+    joint_data = fiber_data_gpu * block_vals
+    
+    # Create result and transfer back to CPU
+    joint_kernel_gpu = cp_sparse.coo_matrix(
+        (joint_data, (fiber_row_gpu, fiber_col_gpu)), 
         shape=fiber_kernel.shape
     )
-    joint_kernel.eliminate_zeros()
+    joint_kernel_gpu.eliminate_zeros()
     
-    return joint_kernel
+    return joint_kernel_gpu
+
 
 
 def symmetrize(mat):
