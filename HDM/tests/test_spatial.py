9e4c78c5aa8b3b8350eceb87764a5e427f60d78e
sofusl90 <sofus@laubel.net>
2025-10-30 21:12:59 +0100

    python 3.10 is now deprecated as newer version of jax is not supported. Also the test is deprectated as we are now using a linear operator

--- HDM/tests/test_spatial.py
+++ HDM/tests/test_spatial.py
@@ -4,33 +4,33 @@ from scipy.sparse import coo_matrix, csr_matrix
 from HDM import cpu
 
 
-def test_compute_joint_kernel():
-    # Define base kernel as CSR (Compressed Sparse Row)
-    base_kernel_data = np.array([[1, 0], [1, 1]])
-    base_kernel = csr_matrix(base_kernel_data)
+# def test_compute_joint_kernel():
+#     # Define base kernel as CSR (Compressed Sparse Row)
+#     base_kernel_data = np.array([[1, 0], [1, 1]])
+#     base_kernel = csr_matrix(base_kernel_data)
 
-    # Define fiber kernel as COO (Coordinate format)
-    fiber_kernel_data = np.array(
-        [[1, 0, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 1, 1, 1]]
-    )
-    fiber_kernel = coo_matrix(fiber_kernel_data)
+#     # Define fiber kernel as COO (Coordinate format)
+#     fiber_kernel_data = np.array(
+#         [[1, 0, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 1, 1, 1]]
+#     )
+#     fiber_kernel = coo_matrix(fiber_kernel_data)
 
-    # Block indices
-    block_indices = np.array([0, 2])
+#     # Block indices
+#     block_indices = np.array([0, 2])
 
-    # Compute joint kernel
-    backend = cpu  # Use the CPU backend
-    joint_kernel = backend.compute_joint_kernel(
-        base_kernel, fiber_kernel, block_indices
-    )
-    joint_kernel_dense = joint_kernel.toarray()
+#     # Compute joint kernel
+#     backend = cpu  # Use the CPU backend
+#     joint_kernel = backend.compute_joint_kernel(
+#         base_kernel, fiber_kernel, block_indices
+#     )
+#     joint_kernel_dense = joint_kernel.toarray()
 
-    # Define expected output
-    expected_joint_kernel = np.array(
-        [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 1, 1, 1]]
-    )
+#     # Define expected output
+#     expected_joint_kernel = np.array(
+#         [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 1, 1, 1]]
+#     )
 
-    # Assertion
-    assert np.array_equal(joint_kernel_dense, expected_joint_kernel), (
-        "Joint kernel output doesn't match expected output"
-    )
+#     # Assertion
+#     assert np.array_equal(joint_kernel_dense, expected_joint_kernel), (
+#         "Joint kernel output doesn't match expected output"
+#     )
